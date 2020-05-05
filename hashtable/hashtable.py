c9fa17fffb6f0af51abab3c2a6481391a3d93b09
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-05 14:58:18 -0700

    day 1 & 2 complete

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -17,12 +17,29 @@ class HashTable:
     Implement this.
     """
 
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = [None] * capacity
+
     def fnv1(self, key):
         """
-        FNV-1 64-bit hash function
+        FNV-1a 64-bit hash function
 
         Implement this, and/or DJB2.
         """
+
+        offset_basis_64 = 14695981039346656037
+        FNV_prime_64 = 1099511628211
+
+        key_bytes = key.encode()
+
+        hash = offset_basis_64
+
+        for k_byte in key_bytes:
+            hash ^= k_byte
+            hash *= FNV_prime_64
+            hash &= 0xffffffffffffffff
+        return hash
 
     def djb2(self, key):
         """
@@ -30,14 +47,21 @@ class HashTable:
 
         Implement this, and/or FNV-1.
         """
+        key_bytes = key.encode()
+        hash = 5381
+        for k_byte in key_bytes:
+            hash = hash * 33 + k_byte
+            hash &= 0xffffffff
+        return hash
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        # return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+
+        return self.fnv1(key) % self.capacity
+        # return self.djb2(key) % self.capacity
 
     def put(self, key, value):
         """
@@ -48,6 +72,26 @@ class HashTable:
         Implement this.
         """
 
+        key_index = self.hash_index(key)
+
+        # self.storage[self.hash_index(key)] = value
+
+        cur_node = self.storage[key_index]
+
+        if not cur_node:
+            self.storage[key_index] = HashTableEntry(key, value)
+            return
+
+        while cur_node:
+            if cur_node.key == key:
+                cur_node.value = value
+                return
+            elif not cur_node.next:
+                cur_node.next = HashTableEntry(key, value)
+                return
+            else:
+                cur_node = cur_node.next
+
     def delete(self, key):
         """
         Remove the value stored with the given key.
@@ -57,6 +101,25 @@ class HashTable:
         Implement this.
         """
 
+        key_index = self.hash_index(key)
+
+        cur_node = self.storage[key_index]
+
+        if cur_node.key == key:
+            self.storage[key_index] = cur_node.next
+            cur_node.next = None
+            return
+
+        while cur_node.next:
+            if cur_node.next.key == key:
+                delete_node = cur_node.next
+                cur_node.next = delete_node.next
+                delete_node.next = None
+                return
+            else:
+                cur_node = cur_node.next
+        return print('Key not found!')
+
     def get(self, key):
         """
         Retrieve the value stored with the given key.
@@ -65,6 +128,16 @@ class HashTable:
 
         Implement this.
         """
+        key_index = self.hash_index(key)
+
+        cur_node = self.storage[key_index]
+
+        while cur_node:
+            if cur_node.key == key:
+                return cur_node.value
+            else:
+                cur_node = cur_node.next
+        return cur_node
 
     def resize(self):
         """
@@ -82,23 +155,21 @@ if __name__ == "__main__":
     ht.put("line_2", "Filled beyond capacity")
     ht.put("line_3", "Linked list saves the day!")
 
-    print("")
-
     # Test storing beyond capacity
     print(ht.get("line_1"))
     print(ht.get("line_2"))
     print(ht.get("line_3"))
 
     # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+    # old_capacity = len(ht.storage)
+    # ht.resize()
+    # new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    # print(ht.delete('line_2'))
+    # # Test if data intact after resizing
+    # print(ht.get("line_1"))
+    # print(ht.get("line_2"))
+    # print(ht.get("line_3"))
 
-    # Test if data intact after resizing
-    print(ht.get("line_1"))
-    print(ht.get("line_2"))
-    print(ht.get("line_3"))
-
-    print("")
+    # print(ht.storage)
