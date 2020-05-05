ca894b711ab9c58b85aa8931ad0b94bff53c1e70
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-05 16:32:30 -0700

    resize complete, changes both up and down, day 3 complete

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -20,6 +20,7 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = [None] * capacity
+        self.load = 0
 
     def fnv1(self, key):
         """
@@ -71,20 +72,29 @@ class HashTable:
 
         Implement this.
         """
+        self.load += 1
+
+        load_factor = self.load / self.capacity
 
         key_index = self.hash_index(key)
         cur_node = self.storage[key_index]
 
         if not cur_node:
             self.storage[key_index] = HashTableEntry(key, value)
+            if load_factor > .75:
+                self.resize(self.capacity * 2)
             return
 
         while cur_node:
             if cur_node.key == key:
                 cur_node.value = value
+                if load_factor > .75:
+                    self.resize(self.capacity * 2)
                 return
             elif not cur_node.next:
                 cur_node.next = HashTableEntry(key, value)
+                if load_factor > .75:
+                    self.resize(self.capacity * 2)
                 return
             else:
                 cur_node = cur_node.next
@@ -102,9 +112,15 @@ class HashTable:
 
         cur_node = self.storage[key_index]
 
+        self.load -= 1
+
+        load_factor = self.load / self.capacity
+
         if cur_node.key == key:
             self.storage[key_index] = cur_node.next
             cur_node.next = None
+            if load_factor < .25:
+                self.resize(self.capacity // 2)
             return
 
         while cur_node.next:
@@ -112,6 +128,8 @@ class HashTable:
                 delete_node = cur_node.next
                 cur_node.next = delete_node.next
                 delete_node.next = None
+                if load_factor < .25:
+                    self.resize(self.capacity // 2)
                 return
             else:
                 cur_node = cur_node.next
@@ -126,7 +144,6 @@ class HashTable:
         Implement this.
         """
         key_index = self.hash_index(key)
-
         cur_node = self.storage[key_index]
 
         while cur_node:
@@ -136,7 +153,7 @@ class HashTable:
                 cur_node = cur_node.next
         return cur_node
 
-    def resize(self):
+    def resize(self, new_capacity=None):
         """
         Doubles the capacity of the hash table and
         rehash all key/value pairs.
@@ -144,29 +161,45 @@ class HashTable:
         Implement this.
         """
 
+        if not new_capacity:
+            return
+
+        dbl_hashtable = HashTable(new_capacity)
+        for i in self.storage:
+            node = i
+            while node:
+                dbl_hashtable.put(node.key, node.value)
+                node = node.next
+        self.capacity = new_capacity
+        self.load = dbl_hashtable.load
+        self.storage = dbl_hashtable.storage
+        del dbl_hashtable
+
 
 if __name__ == "__main__":
     ht = HashTable(2)
-
+    old_capacity = len(ht.storage)
     ht.put("line_1", "Tiny hash table")
     ht.put("line_2", "Filled beyond capacity")
     ht.put("line_3", "Linked list saves the day!")
 
     # Test storing beyond capacity
-    print(ht.get("line_1"))
-    print(ht.get("line_2"))
-    print(ht.get("line_3"))
-
-    # Test resizing
-    # old_capacity = len(ht.storage)
-    # ht.resize()
-    # new_capacity = len(ht.storage)
-
-    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
-    # print(ht.delete('line_2'))
-    # # Test if data intact after resizing
     # print(ht.get("line_1"))
     # print(ht.get("line_2"))
     # print(ht.get("line_3"))
 
-    # print(ht.storage)
+    # Test resizing
+
+    ht.resize(20)
+    new_capacity = len(ht.storage)
+
+    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+
+    ht.delete('line_2')
+    ht.delete('line_1')
+    # # Test if data intact after resizing
+    print(ht.get("line_1"))
+    print(ht.get("line_2"))
+    print(ht.get("line_3"))
+
+    print(len(ht.storage), 'final')
